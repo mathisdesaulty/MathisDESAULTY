c1ae1bbe77a07cff1424a7126b3a7336cd8dc3ea
mathisdesaulty <128808167+mathisdesaulty@users.noreply.github.com>
2024-09-16 17:48:19 +0300

    refactoring of math_tool

--- Object/math_tool.py
+++ Object/math_tool.py
@@ -11,34 +11,62 @@ class MathTool:
     """
 
     @staticmethod
-    def calculate_one_way_distance(coords1, coords2, image1, image2) -> float:
+    def neighboors_positive(coords1, image1, image2):
+        """
+        Check if the given coordinates have positive neighbors in the provided images.
+        Parameters:
+        - coords1 (tuple): The coordinates (x, y) to check for positive neighbors.
+        - image1 (ndarray): The first image.
+        - image2 (ndarray): The second image.
+        Returns:
+        - find (bool): True if positive neighbors are found, False otherwise.
+        """
+        neighbors_offset = [(-1, -1), (-1, 0), (-1, 1),
+                            (0, -1), (0, 0), (0, 1),
+                            (1, -1), (1, 0), (1, 1)]
+        find = False
+        x1, y1 = coords1
+        for x, y in neighbors_offset:
+            x2, y2 = x1 + x, y1 + y
+            if (
+                image2.shape[0] > x2
+                and image2.shape[1] > y2
+                and image2[x2, y2] == image1[x1, y1]
+            ):
+                find = True
+
+        return find
+
+    @staticmethod
+    def same_value(coords1, image1, image2):
+        """
+        Check if the pixel values at the given coordinates in two images are the same.
+        Args:
+            coords1 (list): List of coordinate tuples (x, y).
+            image1 (ndarray): First image.
+            image2 (ndarray): Second image.
+        Returns:
+            bool: True if the pixel values are the same in both images, False otherwise.
+        """
+        for point1 in coords1:
+            x1, y1 = point1
+            if image1[x1, y1] == image2[x1, y1]:
+                return True
+        return False
+
+    @staticmethod
+    def calculate_one_way_distance(coords1,coords2, image1, image2) -> float:
         """
         Calculates the one-way distance between two sets of coordinates.
         Optimized to skip points that have the same value in both images at the same location.
         """
         max_min_dist = 0
-        neighbors_offset = [(-1, -1), (-1, 0), (-1, 1),
-                            (0, -1),(0, 0), (0, 1),
-                            (1, -1), (1, 0), (1, 1)]
         for point1 in coords1:
-            # Extract the corresponding coordinates in both images
-            x1, y1 = point1
             min_dist = np.inf
-            # If the same coordinates in both images have the same value, skip the calculation
-            if image1[x1, y1] == image2[x1, y1]:
+            if MathTool.same_value(coords1, image1, image2):
                 min_dist = 0
-            else: 
-                find = False
-                for x, y in neighbors_offset:
-                    x2, y2 = x1 + x, y1 + y
-                    if (
-                        image2.shape[0] > x2
-                        and image2.shape[1] > y2
-                        and image2[x2, y2] == image1[x1, y1]
-                    ):
-                        find = True
-                        min_dist = 1
-                        break
+            else:
+                find = MathTool.neighboors_positive(point1, image1, image2)
                 if find:
                     min_dist = np.inf
                     for point2 in coords2:
@@ -67,6 +95,6 @@ class MathTool:
         coords1 = np.argwhere(npimage1 == 1)
         coords2 = np.argwhere(npimage2 == 1)
         # Calculate Hausdorff distance in both directions
-        distance1 = MathTool.calculate_one_way_distance(coords1, coords2, npimage1, npimage2)
+        distance1 = MathTool.calculate_one_way_distance(coords1,coords2, npimage1, npimage2)
         distance2 = MathTool.calculate_one_way_distance(coords2, coords1, npimage2, npimage1)
         return float(max(distance1, distance2))
