11cc60ed63af0e1133e8e1d992c5644c65589af9
mathisdesaulty <128808167+mathisdesaulty@users.noreply.github.com>
2024-10-21 15:24:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ from Object.draw_interface import DrawInterface
 
 if __name__ == "__main__":
     root = tk.Tk()
+    print("Creating the dataset...")
     interface = DrawInterface(root)
     root.mainloop()
